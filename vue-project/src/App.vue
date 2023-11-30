ed13ca2014f65bbf318394649cc6a1bfc89dda30
Opris Maria <oprismary_a@yahoo.com>
2023-11-30 23:18:34 +0200

    hotel list and hotel individual page front-end

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,85 +1,38 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <header class="bg-violet-600">
+    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+      <div class="flex lg:flex-1">
+        <RouterLink to="/" class="-m-1.5 p-1.5">
+          <div class="flex items-center space-x-3">
+            <img class="h-16 w-16" src="public/logo.jpg" alt="">
+            <h class="text-3xl text-white font-sans font-bold italic">hotels.</h>
+          </div>
+        </RouterLink>
+      </div>
+      <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+        <RouterLink class="text-sm font-bold text-violet-900 p-2 bg-white rounded-full" to="/new-profile">Sign up
+        </RouterLink>
+        <RouterLink class="text-sm font-bold leading-6 text-white p-2" to="/login">Log in <span
+            aria-hidden="true">&rarr;</span></RouterLink>
+      </div>
+      <div v-if="is_logged_in" class="hidden lg:flex lg:flex-1 lg:justify-end">
+        <RouterLink class="text-sm font-semibold text-white mr-5 rounded-full" to="/profile">
+          <img class="w-12" src="public/avatar/avatar1.png">
+        </RouterLink>
+        <RouterLink class="text-sm font-semibold leading-6 text-gray-900 p-2" to="/login">Log out <span
+            aria-hidden="true">&rarr;</span></RouterLink>
+      </div>
+    </nav>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
   </header>
 
-  <RouterView />
+  <RouterView/>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup lang="ts">
+import {RouterLink, RouterView} from 'vue-router';
+import {ref} from "vue";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+const is_logged_in = ref(false);
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+</script>
